02029f39ca81390f469dcc4fe572f47b13bfb8ae
KUMAR GAURAV SINGH <123926335+kgs6418@users.noreply.github.com>
2023-08-29 20:34:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ require('./db/conn.js')
 app.use(express.json());
 
 app.use(function (req, res, next) {
-res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Origin', 'https://client-instagram.vercel.app');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 res.setHeader('Access-Control-Allow-Credentials', true);
