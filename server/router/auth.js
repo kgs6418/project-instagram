5468398db90d36361480dc9b17186aab49495ec8
KUMAR GAURAV SINGH <123926335+kgs6418@users.noreply.github.com>
2023-08-29 23:12:01 +0530

    Update auth.js

--- server/router/auth.js
+++ server/router/auth.js
@@ -16,7 +16,7 @@ router.get('/',(req,res) =>{
     res.send("hello GAURAV from router")
 })
 app.use(function (req, res, next) {
-res.setHeader('Access-Control-Allow-Origin', 'https://client-instagram.vercel.app');
+res.setHeader('Access-Control-Allow-Origin', 'https://client-instagram.vercel.app/login');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 res.setHeader('Access-Control-Allow-Credentials', true);
