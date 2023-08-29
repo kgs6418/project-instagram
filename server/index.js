410e582bc61f0f0c731b148a0ea6296944b59002
KUMAR GAURAV SINGH <123926335+kgs6418@users.noreply.github.com>
2023-08-29 23:27:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,22 +11,20 @@ dotenv.config({path:'./.env'})
 require('./db/conn.js')
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3001',
+  methods: ['GET', 'POST'],
+  headers: 'Content-Type',
+  credentials: true
+}))
 
-app.use(function (req, res, next) {
-res.setHeader('Access-Control-Allow-Origin', 'https://client-instagram.vercel.app');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-res.setHeader('Access-Control-Allow-Credentials', true);
-
-next();
-});
 
 app.use(require('./router/auth.js'))
 
 const PORT = process.env.PORT || 8000;
 
 
-//connecting to database
+
 
 
 //this is home route
