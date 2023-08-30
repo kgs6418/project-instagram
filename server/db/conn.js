089401e6c3cab8b51a6be3f82f82631b8abb7829
KUMAR GAURAV SINGH <123926335+kgs6418@users.noreply.github.com>
2023-08-30 11:11:08 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const DATABASE=process.env.DATABASE;
 
 
-mongoose.connect(DATABASE)
+mongoose.connect("mongodb+srv://gs201082:gs201082@cluster0.znqwadq.mongodb.net/instadata")
         .then(()=>{console.log('app is  connected to database')})
         .catch((error)=>{'app is not connected'})
