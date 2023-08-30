5c4f419f18fa8d87fc17039da36e47e530b8aec1
KUMAR GAURAV SINGH <123926335+kgs6418@users.noreply.github.com>
2023-08-30 10:49:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ require('./db/conn.js')
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://client-instagram.vercel.app/*',
+    origin: '*',
   methods: ['GET', 'POST'],
   headers: 'Content-Type',
   credentials: true
